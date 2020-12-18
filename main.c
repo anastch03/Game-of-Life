9bd33cb8a556633bc438a8399f34cf65dfb3d98c
Anastasia <achen311@midwoodhighschool.org>
2020-12-18 15:44:07 +0000

    actually 99% complete. can still add some optional flair

--- main.c
+++ main.c
@@ -6,44 +6,48 @@ void update_world(void);
 void print_world(void);
 int check(int height, int width);
 
-//create a 70 by 50 array which would be the "world"
-char cell[70][50]; 
-char new_cell[70][50];
+//create a 50 by 100 array which would be the "world"
+char cell[50][100]; 
+char new_cell[50][100];
 
 int main(void) {
-  
+  printf("\033[2J\033[H"); //clear terminal 
   //initialize array to "-"s (dead)
-  for (int i = 0; i < 70; i++)
+  for (int i = 0; i < 50; i++)
   {
-    for (int j = 0; j < 50; j++)
+    for (int j = 0; j < 100; j++)
     {
       cell[i][j] = '-';
     }
   }
   
   //initialize configuration ie. put some life into the world 
-  cell[0][1] = 'O';
-  cell[4][0] = 'O';
-  cell[1][2] = 'O';
-  cell[1][4] = 'O';
-  cell[4][4] = 'O';
-  cell[0][2] = 'O';
+  cell[24][49] = 'O';
+  cell[24][50] = 'O';
+  cell[24][51] = 'O';
+  cell[23][50] = 'O';
+  cell[21][49] = 'O';
+  cell[21][50] = 'O';
+  cell[21][51] = 'O';
+  cell[20][50] = 'O';
+  cell[19][18] = 'O';
+  cell[19][19] = 'O';
+  cell[19][20] = 'O';
+  cell[18][19] = 'O';
+  cell[18][18] = 'O';
+  cell[18][17] = 'O';
+  cell[17][17] = 'O';
+  cell[17][19] = 'O';
+    
 
   //outer loop for iteration of each cycle
-  for (int i = 0; i < 10; i++) //goes for 10 generations
+  for (int i = 0; i < 40; i++) //goes for 40 generations
   {
     print_world(); //initial configuration
     update_world();
     //write new cell into old cell before the new cell updates
     memcpy(cell, new_cell, sizeof(new_cell)+1);
-    /*for (int i = 0; i < 5; i++)
-    {
-      for (int j = 0; j < 5; j++)
-      {
-        cell[i][j] = new_cell[i][j];
-      }
-    }*/
-    sleep(2);
+    usleep(500000);
     printf("\033[2J\033[H"); //clear terminal 
   }
 }
@@ -51,11 +55,11 @@ int main(void) {
 //print out the world onto terminal
 void print_world(void)
 {
-  for (int column = 0; column < 20; column++)
+  for (int row = 0; row < 50; row++)
   {
-    for (int row = 0; row < 50; row++)
+    for (int column = 0; column < 100; column++)
     {
-      if (row % 50 == 0)
+      if (column % 100 == 0)
         printf("\n");
       printf("%c", cell[row][column]);
     }
@@ -67,9 +71,9 @@ void print_world(void)
 void update_world(void)
 {
   //traverse world
-  for (int i = 0; i < 70; i++)
+  for (int i = 0; i < 50; i++)
   {
-    for (int j = 0; j < 50; j++)
+    for (int j = 0; j < 100; j++)
     {
       switch (check(i, j))
       {
@@ -98,8 +102,8 @@ int check(int height, int width)
     for (int w = width - 1; w <= width + 1; w++)
     {
       //if (h,w) is a surrounding cell, within in bounds of the grid, and is alive 
-      if (!(h == height && w == width) && h >= 0 && h <= 70
-       && w >= 0 && w <= 50 && cell[h][w] == 79)
+      if (!(h == height && w == width) && h >= 0 && h <= 50
+       && w >= 0 && w <= 100 && cell[h][w] == 79)
         alive++;//increment alive count
     }
   } 
